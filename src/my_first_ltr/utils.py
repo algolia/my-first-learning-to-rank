d9111e36589252064a1f4a29c6bdd86626c7476b
RaseMotte <claire.helme-guizon@algolia.com>
2024-12-09 13:45:03 +0100

    feat: add unprocess dataset for explo

--- src/my_first_ltr/utils.py
+++ src/my_first_ltr/utils.py
@@ -1,3 +1,4 @@
+import json
 import os
 from pathlib import Path
 import pandas as pd
@@ -13,3 +14,18 @@ def load_dataset(sample: int = 0, *, local: bool = False) -> pd.DataFrame:
     if sample:
         return pd.read_csv(source).sample(sample)
     return pd.read_csv(source)
+
+
+def load_raw_dataset(local: bool = False) -> pd.DataFrame:
+    """Load dataset with categories not hot encoded for data exploration."""
+    if local:
+        here = Path(__file__)
+        source = here.parent.parent.parent / "data/dataset_with_cats.csv"
+    else:
+        source = "https://raw.githubusercontent.com/algolia/my-first-learning-to-rank/main/data/dataset_with_cats.csv"
+
+    dataset = pd.read_csv(
+        source,
+        converters={"genres": json.loads, "production_countries": json.loads},
+    )
+    return dataset
